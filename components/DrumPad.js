fa8fddef02c586034d2ec109c367cbf276c03a16
Bastiaan van Graafeiland <bastiaan89@live.nl>
2020-06-25 20:12:57 +0200

    complete drum machine

--- components/DrumPad.js
+++ components/DrumPad.js
@@ -1,7 +1,23 @@
-export default function DrumPad({ children, file }) {
+import {useEffect, useRef} from "react";
+
+export default function DrumPad({ children, file, mapping, onPlay }) {
+    const audioEl = useRef()
+    function playSound() {
+        onPlay(file)
+        audioEl.current.play()
+    }
+
+    useEffect(() => {
+        document.addEventListener('keyup', event => {
+            if (event.key.toLowerCase() === mapping) {
+                playSound()
+            }
+        }, )
+    }, [])
+
     return (
-        <button id={file} className="drum-pad bg-gray-200 hover:bg-gray-400 p-4 text-lg font-semibold">
-            <audio id={children} src={file} className="clip" />
+        <button id={file} onClick={playSound} className="drum-pad bg-gray-200 hover:bg-gray-400 p-4 text-lg font-semibold">
+            <audio id={children} src={`/sounds/${file}`} ref={audioEl} className="clip" />
             {children}
         </button>
     )
--- pages/drum-machine.js
+++ pages/drum-machine.js
@@ -1,25 +1,30 @@
 import DrumPad from "../components/DrumPad"
+import {useState} from "react";
 
 const buttons = [
-    { key: 'q', file: 'audio-q' },
-    { key: 'w', file: 'audio-w' },
-    { key: 'e', file: 'audio-e' },
-    { key: 'a', file: 'audio-a' },
-    { key: 's', file: 'audio-s' },
-    { key: 'd', file: 'audio-d' },
-    { key: 'z', file: 'audio-z' },
-    { key: 'x', file: 'audio-x' },
-    { key: 'c', file: 'audio-c' },
+    { key: 'q', file: 'alesis-d4fx-00.wav' },
+    { key: 'w', file: 'alesis-d4fx-01.wav' },
+    { key: 'e', file: 'alesis-d4fx-02.wav' },
+    { key: 'a', file: 'alesis-d4fx-03.wav' },
+    { key: 's', file: 'alesis-d4fx-04.wav' },
+    { key: 'd', file: 'alesis-d4fx-05.wav' },
+    { key: 'z', file: 'alesis-d4fx-06.wav' },
+    { key: 'x', file: 'alesis-d4fx-07.wav' },
+    { key: 'c', file: 'alesis-d4fx-08.wav' },
 ]
 
 export default function DrumMachine() {
+    const [sound, setSound] = useState('')
+
     return (
         <div id="drum-machine" className="w-1/2 m-auto">
-            <div id="display">
-
-            </div>
+            <div id="display">{sound}</div>
             <div className="grid grid-cols-3 gap-3 w-1/3 m-auto mt-10">
-                { buttons.map(b => <DrumPad file={b.file}>{ b.key.toUpperCase() }</DrumPad>) }
+                { buttons.map(b =>
+                    <DrumPad key={b.key} mapping={b.key} file={b.file} onPlay={setSound}>
+                        { b.key.toUpperCase() }
+                    </DrumPad>
+                ) }
             </div>
         </div>
     )
